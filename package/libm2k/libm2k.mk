6d259f3f054af26f6366228547af5157e2945dae
Jorge Marques <jorge.marques@analog.com>
2026-06-29 10:58:41 +0200

    package/libm2k: use raised cmake version sha
    Commit 5e12a2b955390e973473fb5b0993d4eeac2e14dd raises cmake version to
    support cmake 4. Commit 94fb7036f0541e6d655d80610b3eb04aea580a69 from
    the same history updates minor collaterals to support current LTS
    distros.
    
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

--- package/libm2k/libm2k.mk
+++ package/libm2k/libm2k.mk
@@ -3,8 +3,9 @@
 # libm2k
 #
 ################################################################################
-LIBM2K_VERSION = 0.9.0
-LIBM2K_SITE = https://github.com/analogdevicesinc/libm2k/archive/refs/tags/v$(LIBM2K_VERSION)
+LIBM2K_VERSION = 94fb7036f0541e6d655d80610b3eb04aea580a69
+LIBM2K_SITE = https://github.com/analogdevicesinc/libm2k.git
+LIBM2K_SITE_METHOD = git
 
 LIBM2K_INSTALL_STAGING = YES
 LIBM2K_LICENSE = LGPL-2.1
